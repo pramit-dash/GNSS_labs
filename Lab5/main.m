c2b16266add4a0b1b7f2cffb292d0a0cd40f598a
pramit <pramit.dash@tum.de>
2018-01-21 13:30:42 +0100

    Commit to add Xanthi's updates to main.m code

--- Lab5/main.m
+++ Lab5/main.m
@@ -1,3 +1,32 @@
 %% GNSS labs Group2: lab5
 % Main code would come here
-% Oh cool! So everybody can modify the code here right? 
+
+
+%% ------------------ GPS timestamps to seconds ---------------------------
+clear all
+close all
+clc
+
+gps_week = 1499; % gps week
+
+start_gps = 259263.5750014; % gps sec of week of data start
+sat_1_gps = 259267.5750014; % gps sec of week acquisition of first satellite
+TTFF_gps = 259759.5792914; % gps sec of week of TTFF
+
+% conversion to year, month and decimal day
+[yr_start, mn_start, dy_start]= jd2cal(gps2jd(gps_week,start_gps,0))
+[yr_sat_1, mn_sat_1, dy_sat_1]= jd2cal(gps2jd(gps_week,sat_1_gps,0))
+[yr_TTFF, mn_TTFF, dy_TTFF]= jd2cal(gps2jd(gps_week,TTFF_gps,0))
+
+% time in seconds of acquisition of the first satellite
+Sat_1_seconds = (dy_sat_1 - dy_start)*86400;
+
+% time in seconds of TTFF
+TTFF_seconds = (dy_TTFF - dy_start)*86400;
+
+% sampling rate
+first = 259263.5750014;
+second = 259264.5750014;
+[yr,mn,dy]= jd2cal(gps2jd(gps_week,first,0));
+[yr2,mn2,dy2]= jd2cal(gps2jd(gps_week,second,0));
+Rate = (dy2 - dy)*86400
